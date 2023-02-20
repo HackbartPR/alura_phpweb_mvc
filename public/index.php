de7e0b5c36d68dfd3de7ee7c8ff320fa02f40170
HackbartPR <cgharckbart@gmail.com>
2023-02-20 10:12:45 -0300

    Separando por Views

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ use HackbartPR\Repository\PDOVideoRepository;
 use HackbartPR\Controller\NewVideoController;
 use HackbartPR\Controller\SendVideoController;
 use HackbartPR\Controller\ShowVideoController;
+use HackbartPR\Controller\Response404Controller;
 use HackbartPR\Controller\RemoveVideoController;
 use HackbartPR\Controller\UpdateVideoController;
 
@@ -21,27 +22,23 @@ $path = $_SERVER['PATH_INFO'];
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($path === '/' || !array_key_exists('PATH_INFO', $_SERVER)) {
-    $controller = new ShowVideoController($repository, $message);
-        $controller->processRequest();
+    $controller = new ShowVideoController($repository, $message);        
 } else if ($path === '/novo') {
     if ($method === 'GET') {
-        $controller = new SendVideoController($repository, $message);
-        $controller->processRequest();
+        $controller = new SendVideoController($repository, $message);        
     } else if ($method === 'POST') {
-        $controller = new NewVideoController($repository, $message);
-        $controller->processRequest();
+        $controller = new NewVideoController($repository, $message);        
     }
 } else if ($path === '/editar') {
     if ($method === 'GET') {
-        $controller = new SendVideoController($repository, $message);
-        $controller->processRequest();
+        $controller = new SendVideoController($repository, $message);        
     } else if ($method === 'POST') {
-        $controller = new UpdateVideoController($repository, $message);
-        $controller->processRequest();
+        $controller = new UpdateVideoController($repository, $message);        
     }
 } else if ($path === '/remover') {
-    $controller = new RemoveVideoController($repository, $message);
-    $controller->processRequest();
+    $controller = new RemoveVideoController($repository, $message);    
 } else {
-    http_response_code(404);
+    $controller = new Response404Controller();
 }
+
+$controller->processRequest();
